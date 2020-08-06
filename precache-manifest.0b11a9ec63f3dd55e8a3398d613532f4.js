55392e527436c855eb573e3ac4516b8e9d55a17e
Naman Saini <saininaman98@gmail.com>
2020-08-07 02:27:13 +0530

    Updates

---
+++ precache-manifest.0b11a9ec63f3dd55e8a3398d613532f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f3f71fdf510161344fdf81d303bdb57",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "bee368a0a38236a451d7",
+    "url": "/react/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "d98e3163103350e0ac07",
+    "url": "/react/static/css/main.118872c6.chunk.css"
+  },
+  {
+    "revision": "bee368a0a38236a451d7",
+    "url": "/react/static/js/2.cffb6ff2.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/react/static/js/2.cffb6ff2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d98e3163103350e0ac07",
+    "url": "/react/static/js/main.4b938b6c.chunk.js"
+  },
+  {
+    "revision": "6aa86b47113ca79e90b5",
+    "url": "/react/static/js/runtime-main.84dbb5e9.js"
+  }
+]);
